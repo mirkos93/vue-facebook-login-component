7b549b82316f9252362fc7d16ea50412e94b77c8
Mirko Prota <mirkos93@gmail.com>
2020-02-26 19:31:26 +0100

    Fix in-canvas navigation
    Reading the docs (https://developers.facebook.com/docs/reference/javascript/FB.login/v6.0#login-dialog) it was specified: on Canvas, if the user is unknown (that is, when the login status == 'unknown'), do not use the JS SDK to sign in the user, instead do a full redirect of the page to the login dialog so that we get a consistent state.

--- src/components/FBLogin.Scope.js
+++ src/components/FBLogin.Scope.js
@@ -120,6 +120,11 @@ export default {
       const response = await this.doAsync(login)
       if (response.status === 'connected') {
         this.connected = true
+      } else if(response.status === 'unknown' || response.status === 'not_authorized'){
+        window.location.href = 'https://www.facebook.com/dialog/oauth?client_id=' + this.appId
+                                  + '&redirect_uri=' + window.location.href;
+        this.connected = false;
+        return false;
       } else {
         this.connected = false
       }
